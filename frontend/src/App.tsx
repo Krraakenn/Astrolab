19b0a74c54e229535744c0db44eb18bf0a02c5f5
Tristan <tristan@fopagacher.com>
2024-09-17 13:26:24 +0200

    [MOD] Update styling in App component and add Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="box">
-      <div>
-        <p id="astrolab">
-          <span>A</span>
-          <span>S</span>
-          <span>T</span>
-          <span>R</span>
-          <span>O</span>
-          <span>L</span>
-          <span>A</span>
-          <span>B</span>
-        </p>
-      </div>
-      <img className="logo" src={logo} alt="logo" />
+      
     </div>
   );
 }
